65b0a5ff892c145ea62eefd5c5e7376e0fdbba91
Soheil Nikzad <soheil.nikzad@gmail.com>
2025-07-06 14:18:38 +0330

    Update eth-sig-util-browser.js

--- cid/eth-sig-util-browser.js
+++ cid/eth-sig-util-browser.js
@@ -1,1 +1,27 @@
-h
+window.ethSigUtil = {
+  encrypt: function(publicKey, dataObj, version) {
+    if (!window.nacl || !window.naclUtil) {
+      throw new Error("nacl or naclUtil not loaded in window.");
+    }
+
+    const nacl = window.nacl;
+    const naclUtil = window.naclUtil;
+
+    const ephemKeyPair = nacl.box.keyPair();
+    const msgParams = naclUtil.decodeUTF8(dataObj.data);
+    const nonce = nacl.randomBytes(24);
+    const encryptedMessage = nacl.box(
+      msgParams,
+      nonce,
+      naclUtil.decodeBase64(publicKey),
+      ephemKeyPair.secretKey
+    );
+
+    return {
+      version: version,
+      ephemPublicKey: naclUtil.encodeBase64(ephemKeyPair.publicKey),
+      nonce: naclUtil.encodeBase64(nonce),
+      ciphertext: naclUtil.encodeBase64(encryptedMessage)
+    };
+  }
+};
