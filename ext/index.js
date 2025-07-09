6cc9bdf28b333fdb9f151fd9a22de7ea43b223a6
Soheil Nikzad <soheil.nikzad@gmail.com>
2025-07-09 14:56:53 +0330

    Update index.js

--- ext/index.js
+++ ext/index.js
@@ -1,45 +1,36 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const inputText = document.getElementById("inputText");
   const createBtnAdd = document.getElementById("createBtnAdd");
   const storedDataElement = document.getElementById("storedData");
 
-  
-  let tags = {}
+  let tags = {};
 
-  // loading existing tags from storage or initailize an empty object
+  // بارگذاری تگ‌ها از localStorage
+  const savedTags = localStorage.getItem("tags");
+  if (savedTags) {
+    tags = JSON.parse(savedTags);
+  }
 
-  chrome.storage.local.get("tags", function (data) {
-    if (data.tags) tags = JSON.parse(data.tags);
-    displayTags() //calling the tags
-  })
+  displayTags();
 
-  displayTags()
-
-  createBtnAdd.addEventListener("click", function() {
+  createBtnAdd.addEventListener("click", function () {
     const tag = inputText.value.trim();
 
-    if(tag !== ""){
-      tags[tag] = [] // add new tag to the new object
-      chrome.runtime.sendMessage(
-        {
-          type : "STORE",
-          tags
-        }
-      )
-      displayTags()
-      inputText.value = ""
+    if (tag !== "") {
+      tags[tag] = [];
+      localStorage.setItem("tags", JSON.stringify(tags));
+      displayTags();
+      inputText.value = "";
     }
   });
 
-  // display the tags
   function displayTags() {
-    storedDataElement.innerHTML = ""
+    storedDataElement.innerHTML = "";
     for (let tag in tags) {
       if (tags.hasOwnProperty(tag)) {
-        const tagElement = document.createElement("div")
-        tagElement.textContent = tag
-        storedDataElement.appendChild(tagElement)
+        const tagElement = document.createElement("div");
+        tagElement.textContent = tag;
+        storedDataElement.appendChild(tagElement);
       }
     }
   }
