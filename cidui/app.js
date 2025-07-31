693d7ab2417d09ea7879f457b0883b7057c29190
Soheil Nikzad <soheil.nikzad@gmail.com>
2025-07-31 19:39:34 +0330

    Update app.js

--- cidui/app.js
+++ cidui/app.js
@@ -660,7 +660,7 @@ async function decryptContacts(userAddress) {
     
     // Use Etherscan API to get all transactions from user's wallet
     console.log('Making API call to get user transactions...');
-    const response = await fetch(`https://api.etherscan.io/v2/api?chainid=137&module=account&action=txlist&address=${userAddress}&startblock=${fromBlock}&endblock=99999999&sort=desc&apikey=173I6KJ1QIKXC4M5Z9KE43ZF17CD2JK7YD`);
+    const response = await fetch(`https://api.etherscan.io/v2/api?chainid=137&module=account&action=txlist&address=${userAddress}&startblock=${fromBlock}&endblock=99999999&sort=desc&apikey=HQBYRBSTNGP1QCAVQT6FXTDPWPAFPH7G27`);
     const data = await response.json();
     console.log('Raw API response:', data);
     
